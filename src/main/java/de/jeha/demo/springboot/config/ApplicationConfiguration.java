6c627d90de7907a60119afe8e4043556d9b572ac
Jens Hadlich <jenshadlich@googlemail.com>
2016-09-16 15:04:59 +0200

    prepare Spring Boot app skeleton

---
+++ src/main/java/de/jeha/demo/springboot/config/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package de.jeha.demo.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+@Configuration
+public class ApplicationConfiguration {
+
+}
